e4197fb957f3f55eefb006b30f539b4e379a4f34
Beam-ki <skunn123@gmail.com>
2022-09-22 21:32:50 +0900

    Django 1주2주차 수업내용

---
+++ project_02.py
@@ -0,0 +1,20 @@
+class login():
+    id = ''
+    title = ''
+    author = ''
+    content = ''
+
+
+Login = login()
+Login.id='A'
+Login.title='AA'
+Login.author='AAA'
+Login.content='AAAA'
+
+print(Login)
+print(Login.id)
+print(Login.title)
+print(Login.author)
+print(Login.content)
+
+
