09c69e92975874bcac663f9d0e09cabf9af18396
Beam-ki <skunn123@gmail.com>
2022-09-28 11:04:48 +0900

    3주차

--- user/views.py
+++ user/views.py
@@ -1,10 +1,42 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from .models import UserModel
+from django.http import HttpResponse
+from django.contrib.auth import get_user_model  # 사용자가 데이터 베이스안에있는지 검사하는 함수
+from django.contrib import auth
 
 
 # Create your views here.
 def sign_up_view(request):
-    return render(request, 'user/signup.html')
+    if request.method == 'GET':
+        return render(request, 'user/signup.html')
+    elif request.method == 'POST':
+        # me = UserModel.objects.get(username=username)
+        username = request.POST.get('username', None)
+        password = request.POST.get('password', None)
+        password2 = request.POST.get('password2', None)
+        bio = request.POST.get('bio', None)
+        if password != password2:
+            return render(request, 'user/signup.html')
+        else:
+            exist_user = get_user_model().objects.filter(username=username)
+            if exist_user:
+                return render(request, 'user/signup.html')
+            else:
+                UserModel.objects.create_user(username=username, password=password, bio=bio)
+                return redirect('/sign-in')
 
 
 def sign_in_view(request):
-    return render(request, 'user/signin.html')
+    if request.method == 'POST':
+        username = request.POST.get('username', None)
+        password = request.POST.get('password', None)
+
+        me = auth.authenticate(request, username=username, password=password)
+        if me is not None:
+            auth.login(request, me)
+            return HttpResponse('로그인 성공!' + username + '님 반갑 습니다')
+        else:
+            return redirect('/sign-in')
+    elif request.method == 'GET':
+
+        return render(request, 'user/signin.html')
